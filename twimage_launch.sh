7e264e3802db67fabedbf4c34d7d6d7a76262fd3
Mat Gilbert <freefallertam@gmail.com>
2012-04-26 15:51:31 +0100

    Initial commit

---
+++ twimage_launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ $# -lt 1 ] ; then
+  echo "supply application name as first argument"
+  exit -1
+fi
+application_path="~/Documents/RailsApps/$1"
+gnome-terminal --tab --title="Redcar" -e "sh -c 'command ~/Documents/Scripts/start_redcar.sh $application_path'" --tab --working-directory=$application_path --title="Server" --command "rails s" --tab --working-directory=$application_path --title="Spork" --command "bundle exec spork" --tab --working-directory=$application_path --title="Autotest" --command "autotest" --tab --working-directory=$application_path --title="Rails" 
