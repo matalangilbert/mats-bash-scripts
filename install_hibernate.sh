16ae73bda15b910a424521c1b184752fda4f54b8
Mat Gilbert <freefallertam@gmail.com>
2012-05-11 19:34:06 +0100

    Hibernate scripts

---
+++ install_hibernate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp hib-script.sh /usr/bin/hib-script.sh
+chmod +x /usr/bin/hib-script.sh
--- start_redcar.sh
+++ start_redcar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  source ~/.bashrc  
- /home/mat/.rvm/gems/ruby-1.9.3-p125/bin/redcar $1
+ /home/mat/.rvm/gems/ruby-1.9.3-p194/bin/redcar $1
 
